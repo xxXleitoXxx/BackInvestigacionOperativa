6d4332f124789e7feb5af8e35330a2e36bcfb534
facu-1538 <facundoaracena@gmail.com>
2025-05-17 10:16:48 -0300

    push facu

--- src/main/java/org/example/entity/Articulo.java
+++ src/main/java/org/example/entity/Articulo.java
@@ -1,7 +1,14 @@
 package org.example.entity;
-
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.Date;
 
+@Entity
+@Data
+@Getter
+@Setter
 public class Articulo {
 
     private String codArt;
