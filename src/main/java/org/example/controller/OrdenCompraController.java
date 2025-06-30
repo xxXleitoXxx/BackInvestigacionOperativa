940ad651b0b1ce22d31f9902d424408f6e163a1c
facu-1538 <facundoaracena@gmail.com>
2025-06-29 23:32:30 -0300

    OC y Venta terminada

--- src/main/java/org/example/controller/OrdenCompraController.java
+++ src/main/java/org/example/controller/OrdenCompraController.java
@@ -110,7 +110,7 @@ public class OrdenCompraController extends BaseControllerImpl<OrdenCompra, Orden
                 OrdenCompra ordenCompraE = new OrdenCompra();
                 ordenCompraE = ordenCompraServiceImp.DTOaOC(ordenCompra);
                 servicio.save(ordenCompraE);
-                ordenCompraServiceImp.verSiLoteOptimo(ordenCompraE);
+
                 ordenCompraServiceImp.verSiLoteOptimo(ordenCompraE);
                 return ResponseEntity.ok(ordenCompraE);
 
